ebfa5a1c68ccc1b69aa97632170adbf0e97c88b0
jhamlal <jhamlal.sahu92@gmail.com>
2022-02-14 14:13:38 +0530

    upload files

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,118 @@
+<?php  
+namespace App\Http\Controllers;
+   
+use App\Models\Contact;
+use Illuminate\Http\Request;
+  
+class ContactController extends Controller   
+{
+    /**
+		* Create a new controller instance.
+		*
+		* @return void 
+	*/
+	public function __construct()
+	{
+		$this->middleware('auth');
+	}
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $contacts = Contact::latest()->paginate(5);
+    
+        return view('contacts.index',compact('contacts'))
+            ->with('i', (request()->input('page', 1) - 1) * 5);
+    }
+     
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('contacts.create');
+    }
+    
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'phone' => 'required|min:10|max:13',
+        ]);
+    
+        Contact::create($request->all());
+     
+        return redirect()->route('contacts.index')
+                        ->with('success','Contact created successfully.');
+    }
+     
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Contact $contact)
+    {
+        return view('contacts.show',compact('contact'));
+    } 
+     
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Contact $contact)
+    {
+        return view('contacts.edit',compact('contact'));
+    }
+    
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Contact $contact)
+    {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email',
+            'phone' => 'required|min:10|max:13',
+        ]);
+    
+        $contact->update($request->all());
+    
+        return redirect()->route('contacts.index')
+                        ->with('success','Contact updated successfully');
+    }
+    
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Contact  $contact
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Contact $contact)
+    {
+        $contact->delete();
+    
+        return redirect()->route('contacts.index')
+                        ->with('success','Contact deleted successfully');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes([
+    'register' => false, // Registration Routes...
+    'reset' => false, // Password Reset Routes...
+    'verify' => false, // Email Verification Routes...
+]);
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('admin/home', [App\Http\Controllers\HomeController::class, 'adminHome'])->name('admin.home')->middleware('is_admin');
+
+Route::resource('contacts', ContactController::class);
